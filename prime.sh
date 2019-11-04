dfcde6a4d013d652a9bdcdc2fddbe26c808c35d9
DiptoChakrabarty <diptochuck123@gmail.com>
2019-11-04 19:48:18 +0530

    primenos

---
+++ prime.sh
@@ -0,0 +1,26 @@
+echo "Enter Your Range"
+read n
+i=1
+j=0
+
+for ((i=2;i<=n;i++))
+do
+    flag=0
+    for ((j=2;j<i;j++))
+    do
+        rem=$(( $i % $j ))
+        
+        if [ $rem -eq 0 ]
+        then
+            flag=1
+        fi
+     #   echo "$rem $i $j $flag"
+    done
+    if [ $flag -eq 0 ]
+    then
+        echo " Prime Number $i"
+    else
+        echo "Not Prime Number $i"
+    fi
+done
+echo "Over Program"
---
+++ singleprime.sh
@@ -0,0 +1,19 @@
+echo "Enter Your No"
+read n 
+i=2
+flag=0
+for ((i=2;i<=n/2;i++))
+do
+    rem=$(( $n % $i ))
+    if [ $rem -eq 0 ]
+    then 
+        flag=1
+    fi
+done
+
+if [ $flag -eq 1 ]
+then
+    echo "Not Prime No $n"
+else
+    echo "Prime $n"
+fi
