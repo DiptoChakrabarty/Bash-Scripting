626642cbce321a1f14b9882dab9fa3a3cbb0540e
DiptoChakrabarty <diptochuck123@gmail.com>
2019-11-04 20:12:52 +0530

    made sum

--- digitsum.sh
+++ digitsum.sh
@@ -2,11 +2,12 @@ echo "Enter your no"
 read n 
 m=n 
 sum=0
-while [ $n >0 ]
+while [ $n -gt 0 ]
 do
     rem=$(( $n % 10 ))
     n=$(( $n / 10))
+    #echo "$n $rem"
     sum=$(( $sum + $rem ))
-    echo "GOT"
+
 done
 echo "Sum is $sum"
